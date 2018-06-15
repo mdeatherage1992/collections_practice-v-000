4379a70b6bb1da06d381ae73c840d01c17907c84
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 19:20:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,11 +11,16 @@ end
 end
 
 
-
-
-
-
 def sort_array_desc(array)
+  array.sort do |a, b|
+  if a == b
+    0
+  elsif b < a
+    -1
+  elsif b > a
+    1
+  end
+end
 end
 
 def sort_array_char_count(array)
