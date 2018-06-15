7ca313c8ac5269f9df45923a7c7a7512d0b331bb
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 19:43:14 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,79 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+  if a == b
+    0
+  elsif b > a
+    -1
+  elsif b < a
+    1
+  end
+end
+end
 
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+  if a == b
+    0
+  elsif b < a
+    -1
+  elsif b > a
+    1
+  end
+end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+  if a.length == b.length
+    0
+  elsif b.length > a.length
+    -1
+  elsif b.length < a.length
+    1
+  end
+end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+  if a == array[1] && b == array[2]
+    1
+  else
+    -1
+end
+end
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
+end
+return array
+end
+
+def find_a(array)
+  array.select do |name|
+   name.start_with?('a')
+end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum += num
+end
+return sum
+end
+
+def add_s(array)
+  array.each do |ele|
+    if ele != array[1]
+  puts ele << "s"
+end
+end
+end
